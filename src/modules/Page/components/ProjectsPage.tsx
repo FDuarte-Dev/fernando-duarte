d6defefb232b996dc8ee8e3cb713aa9867f455b8
SirNanwar <fernando.guerra.duarte@gmail.com>
2021-03-02 09:03:21 +0000

    Added repositories to Projects page

--- src/modules/Page/components/ProjectsPage.tsx
+++ src/modules/Page/components/ProjectsPage.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { GitHubRepo } from '../model/model';
+import { GitHubServices } from '../services/GitHubServices';
 
-export default class ProjectsPage extends React.Component {
+interface ProjectsPageProps {
+
+}
+
+interface ProjectsPageState {
+    repos: GitHubRepo[];
+    loading: boolean;
+}
+
+export default class ProjectsPage extends React.Component<ProjectsPageProps, ProjectsPageState> {
+    constructor(props: ProjectsPageProps) {
+        super(props);
+        this.state = {
+            repos: [],
+            loading: true
+        }
+    }
+
+    componentDidMount() {
+        const USERNAME = 'FDuarte-Dev';
+
+        GitHubServices.getUserRepositories(USERNAME, (res: any, err: any) => {
+            if (res && res.status === 200) {
+                this.setState({repos: res.data, loading: false});
+                this.forceUpdate();
+            }
+        });
+    }
+    
     render() {
+        let {repos, loading} = this.state;
         return (
-            <h1>Projects Page Component</h1>
+            <>
+            {loading && 
+                <h1>Loading Home Page Component</h1>
+            }
+            {!loading &&
+                <>
+                    <h1>Number of repos: {repos.length}</h1>
+                </>
+            }
+            </>
         );
     }
 }
--- src/modules/Page/model/model.ts
+++ src/modules/Page/model/model.ts
@@ -2,3 +2,13 @@ export interface GitHubUser {
     login: string;
     avatar_url: string;
 }
+
+export interface GitHubRepo {
+    name: string;
+    full_name: string;
+    html_url: string;
+    description: string;
+    created_at: string;
+    updated_at: string;
+    language: string;
+}
