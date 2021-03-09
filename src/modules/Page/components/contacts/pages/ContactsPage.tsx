6b4145bc474a014f43f2e076ddf3ea25c1a59daf
SirNanwar <fernando.guerra.duarte@gmail.com>
2021-03-09 18:12:37 +0000

    Added contacts

--- src/modules/Page/components/contacts/pages/ContactsPage.tsx
+++ src/modules/Page/components/contacts/pages/ContactsPage.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default class ContactsPage extends React.Component {
     render() {
         return (
-            <h1>Contacts Page Component</h1>
+            <>
+                <h3>Phone: </h3><a href="tel:+351916077957">+351 916077957</a>
+                <h3>Email: </h3><a href="mailto:fernando.guerra.duarte@gmail.com">fernando.guerra.duarte@gmail.com</a>
+                <br></br>
+                <a href="https://github.com/FDuarte-Dev">
+                    <FontAwesomeIcon icon={faGithubSquare} size="3x" />
+                </a>
+                <br></br>
+                <a href="https://www.linkedin.com/in/fernando-guerra-duarte/">
+                    <FontAwesomeIcon icon={faLinkedin} size="3x" />
+                </a>
+            </>
         );
     }
 }
