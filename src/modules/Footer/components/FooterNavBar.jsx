285ffe2ea179570bb35cd37099ee80c7fd67d138
SirNanwar <fernando.guerra.duarte@gmail.com>
2021-03-12 18:47:21 +0000

    Added contacts to footer and new fontawesome icons

--- src/modules/Footer/components/FooterNavBar.jsx
+++ src/modules/Footer/components/FooterNavBar.jsx
@@ -4,6 +4,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { 
+    faEnvelope
+  } from '@fortawesome/free-regular-svg-icons';
 
 export default class FooterNavBar extends React.Component {
     render() {
@@ -14,6 +17,10 @@ export default class FooterNavBar extends React.Component {
                         <Nav >FDuarte-Dev @ {new Date().getFullYear()}</Nav>
                         <Nav >
                             <div>
+                                <a href="mailto:fernando.guerra.duarte@gmail.com">
+                                    <FontAwesomeIcon icon={faEnvelope} size="2x" />
+                                    fernando.guerra.duarte@gmail.com
+                                </a>
                                 <a href="https://github.com/FDuarte-Dev">
                                     <FontAwesomeIcon icon={faGithubSquare} size="2x" />
                                 </a>
