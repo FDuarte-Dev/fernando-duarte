1f5cbf3141ac64aaefe4d2ae05e23b0e443c4b96
SirNanwar <fernando.guerra.duarte@gmail.com>
2021-03-23 13:06:17 +0000

    Changed company logo size

--- src/modules/Page/components/workexperience/cards/WorkExperienceCard.tsx
+++ src/modules/Page/components/workexperience/cards/WorkExperienceCard.tsx
@@ -1,44 +1,56 @@
-import React from 'react';
-import { Button, Card } from 'react-bootstrap';
-import { Work } from '../models/models';
+import React from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { Work } from "../models/models";
 
-interface WorkExperienceCardProps{
-    work: Work
+interface WorkExperienceCardProps {
+    work: Work;
 }
 
 interface WorkExperienceCardState {}
 
 export default class WorkExperienceCard extends React.Component<WorkExperienceCardProps, WorkExperienceCardState> {
-
     render() {
-        let { 
-            company_name, 
-            company_logo, 
-            title, 
-            start_date, 
-            end_date, 
-            location, 
-            notable_tasks, 
-            technologies } = this.props.work;
+        let {
+            company_name,
+            company_logo,
+            title,
+            start_date,
+            end_date,
+            location,
+            notable_tasks,
+            technologies,
+        } = this.props.work;
         return (
             <>
-                <Card border='info' style={{flex: 1, width: '80%'}}>
+                <Card border="info" style={{ flex: 1, width: "80%" }}>
                     <Card.Header>
-                    <Card.Img variant="top" src={company_logo} style={{width: '20%'}}/>
-                        <h3>
-                            {title} @ {company_name}
-                        </h3>
-                        <h5>
-                            {location}
-                        </h5>
-                        <h6>
-                            {start_date} - {end_date || 'Ongoing'}
-                        </h6>
+                        <Container>
+                            <Row>
+                                <Col xs={3}>
+                                    <Row xs={1}>
+                                        <Card.Img
+                                            variant="top"
+                                            src={company_logo}
+                                            style={{ paddingTop: "5%" }}
+                                        />
+                                    </Row>
+                                </Col>
+                                <Col>
+                                    <h3>
+                                        {title} @ {company_name}
+                                    </h3>
+                                    <h5>{location}</h5>
+                                    <h6>
+                                        {start_date} - {end_date || "Ongoing"}
+                                    </h6>
+                                </Col>
+                            </Row>
+                        </Container>
                     </Card.Header>
                     <Card.Body>
-                        <Card.Text>{technologies.join(', ')}</Card.Text>
+                        <Card.Text>{technologies.join(", ")}</Card.Text>
                         {notable_tasks.map((task) => {
-                            return <Card.Text>-{task}</Card.Text>
+                            return <Card.Text>-{task}</Card.Text>;
                         })}
                     </Card.Body>
                 </Card>
