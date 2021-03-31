f5611e48800034308c154ac82cc9a181760c7e69
SirNanwar <fernando.guerra.duarte@gmail.com>
2021-03-31 14:11:31 +0100

    Added theming to modals

--- src/modules/Page/components/academic/modals/CurriculumModal.tsx
+++ src/modules/Page/components/academic/modals/CurriculumModal.tsx
@@ -1,42 +1,53 @@
-import React, { useState } from 'react';
-import { Button, CardDeck, Modal } from 'react-bootstrap';
+import React, { useState } from "react";
+import { store } from "../../../../../redux/store";
+import { Button, CardDeck, Modal } from "react-bootstrap";
 import { Discipline } from "../models/model";
-import DisciplineCard from '../cards/DisciplineCard';
+import DisciplineCard from "../cards/DisciplineCard";
 
 interface CurriculumModalProps {
-    curriculum: Discipline[]
+    curriculum: Discipline[];
 }
 
 export const CurriculumModal: React.FC<CurriculumModalProps> = (props) => {
+    const [show, setShow] = useState(false);
 
-  const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    const themeClassname =
+        store.getState().theme === "dark"
+            ? "academic-dark-modal"
+            : "academic-light-modal";
 
-  return (
-    <>
-      <Button variant="outline-info" onClick={handleShow}>
-        Curriculum
-      </Button>
+    return (
+        <>
+            <Button variant="outline-info" onClick={handleShow}>
+                Curriculum
+            </Button>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Curriculum</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <CardDeck style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                {props.curriculum.map((discipline) => {
-                    return <DisciplineCard discipline={discipline} />
-                })}
-            </CardDeck>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
+            <Modal className={themeClassname} show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Curriculum</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <CardDeck
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        {props.curriculum.map((discipline) => {
+                            return <DisciplineCard discipline={discipline} />;
+                        })}
+                    </CardDeck>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+};
--- src/modules/Page/components/academic/modals/ProjectsModal.tsx
+++ src/modules/Page/components/academic/modals/ProjectsModal.tsx
@@ -1,44 +1,49 @@
-import React, { useState } from 'react';
-import { Button, CardDeck, Modal } from 'react-bootstrap';
-import ProjectCard from '../cards/ProjectCard';
+import React, { useState } from "react";
+import { store } from "../../../../../redux/store";
+import { Button, CardDeck, Modal } from "react-bootstrap";
+import ProjectCard from "../cards/ProjectCard";
 import { Project } from "../models/model";
 
 interface ProjectsModalProps {
-    projects: Project[]
+    projects: Project[];
 }
 
 export const ProjectsModal: React.FC<ProjectsModalProps> = (props) => {
+    const [show, setShow] = useState(false);
 
-  const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    const themeClassname =
+        store.getState().theme === "dark"
+            ? "academic-dark-modal"
+            : "academic-light-modal";
 
-  return (
-    <>
-      <Button variant="outline-info" onClick={handleShow}>
-      Project{props.projects.length > 1 ? 's' : ''}
-      </Button>
+    return (
+        <>
+            <Button variant="outline-info" onClick={handleShow}>
+                Project{props.projects.length > 1 ? "s" : ""}
+            </Button>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>
-            Project{props.projects.length > 1 ? 's' : ''}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <CardDeck>
-                {props.projects.map((project) => {
-                    return <ProjectCard project={project} />
-                })}
-            </CardDeck>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
+            <Modal className={themeClassname} show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        Project{props.projects.length > 1 ? "s" : ""}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <CardDeck>
+                        {props.projects.map((project) => {
+                            return <ProjectCard project={project} />;
+                        })}
+                    </CardDeck>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+};
