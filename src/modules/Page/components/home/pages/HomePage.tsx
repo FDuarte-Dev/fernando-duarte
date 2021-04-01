23099908f75682ca5145b4f1cf305f8420d1f937
SirNanwar <fernando.guerra.duarte@gmail.com>
2021-04-01 13:45:01 +0100

    Changed HomePage to use header icon

--- src/modules/Page/components/home/pages/HomePage.tsx
+++ src/modules/Page/components/home/pages/HomePage.tsx
@@ -1,60 +1,20 @@
 import React from 'react';
-import { GITHUB_USERNAME } from '../../../../../utils/constants';
-import { GitHubUser } from '../models/model';
-import { GitHubServices } from '../services/GitHubServices';
-import LoadingPage from '../../loading/pages/LoadingPage';
+import { FACE_LOGO } from '../../../../../utils/constants';
 import { Container } from 'react-bootstrap';
 
-interface HomePageProps {
-
-}
-
-interface HomePageState {
-    user: GitHubUser;
-    loading: boolean;
-}
-
-export default class HomePage extends React.Component<HomePageProps, HomePageState> {
-
-    constructor(props: HomePageProps) {
-        super(props);
-        this.state = {
-            user: {
-                login: '',
-                avatar_url: '',
-            },
-            loading: true
-        };
-    }
-
-    componentDidMount() {
-        const USERNAME = GITHUB_USERNAME;
-
-        GitHubServices.getUser(USERNAME, (res: any, err:any) => {
-            if (res && res.status === 200) {
-                this.setState({user: res.data, loading: false});
-                this.forceUpdate();
-            }
-        });
-    }
+export default class HomePage extends React.Component {
 
     render() {
-        let { user, loading } = this.state;
         return (
-            <>
-            {loading && <LoadingPage />}
-            {!loading &&
-                <Container className='page-container'>
-                    <img className="my_face" src={user.avatar_url} alt='user'/>
-                    <h3>Welcome to my page!</h3>
-                    My name is Fernando Duarte and I'm a Software Developer from Portugal. 
-                    <br></br>
-                    In this webpage you can find a small overview of my progresses in the world of programming.
-                    <br></br>
-                    Feel free to explore further, and to hit me up using any of the contacts below.
-                </Container>
-            }
-            </>
+            <Container className='page-container'>
+                <img className="my_face" src={FACE_LOGO} alt='user'/>
+                <h3>Welcome to my page!</h3>
+                My name is Fernando Duarte and I'm a Software Developer from Portugal. 
+                <br></br>
+                In this webpage you can find a small overview of my progresses in the world of programming.
+                <br></br>
+                Feel free to explore further, and to hit me up using any of the contacts below.
+            </Container>
         );
     }
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,6 +5,8 @@ export const GITHUB_USERNAME = 'FDuarte-Dev';
 export const PLAY_STORE_BUTTON = `${process.env.PUBLIC_URL}/assets/buttons/en_badge_play_store.png`;
 
 // Logos
+export const FACE_LOGO = `${process.env.PUBLIC_URL}/favicon.ico`;
+
 export const CALCULADORA_VOCAL_LOGO = `${process.env.PUBLIC_URL}/assets/logos/application/CalculadoraVocalLogo.PNG`;
 export const DECISION_MAKER_LOGO = `${process.env.PUBLIC_URL}/assets/logos/application/DecisionMakerLogo.png`;
 export const MORSE_TRANSLATOR_LOGO = `${process.env.PUBLIC_URL}/assets/logos/application/MorseTranslatorLogo.png`;
