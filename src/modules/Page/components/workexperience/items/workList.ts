ea6dd963240700b7b38c150f70e47cc49e873e8f
SirNanwar <fernando.guerra.duarte@gmail.com>
2021-04-01 13:57:20 +0100

    Moved most recent work experience to top

--- src/modules/Page/components/workexperience/items/workList.ts
+++ src/modules/Page/components/workexperience/items/workList.ts
@@ -5,30 +5,6 @@ import { COLLAB_LOGO , MORPHIS_LOGO } from '../../../../../utils/constants';
 
 export const WORKS: Work[] = [
     {
-        company_name: 'Collab S.A.',
-        company_logo: COLLAB_LOGO,
-        location: 'Lisbon, Portugal',
-        title: 'Software Engineer',
-        start_date: moment(new Date(2018, 7)).format("MMM YYYY"),
-        end_date: moment(new Date(2020, 7)).format("MMM YYYY"),
-        technologies: [
-            'C#',
-            'C++',
-            'Javascript',
-            '.NET',
-            'Entity Framework',
-            'SQL Server',
-            'Azure DevOps',
-            'Selenium WebDriver'
-        ],
-        notable_tasks: [
-            'Developed REST microservice for asynchronous messaging.',
-            'Implemented real-time encryption on a video call service.',
-            'Developed integration and functional automated testing, guaranteeing continuous integration.',
-            'Worked on development of VoIP call related functionalities using the SIP protocol'
-        ]
-    },
-    {
         company_name: 'Morphis Technologies',
         company_logo: MORPHIS_LOGO,
         location: 'Lisbon, Portugal',
@@ -50,6 +26,30 @@ export const WORKS: Work[] = [
             'REST API development and documentation for various business processes',
             'Implemented generator to create React components from provider models',
             'Developed Rappid diagram visualization tool for React applications'
+        ]
+    },
+    {
+        company_name: 'Collab S.A.',
+        company_logo: COLLAB_LOGO,
+        location: 'Lisbon, Portugal',
+        title: 'Software Engineer',
+        start_date: moment(new Date(2018, 7)).format("MMM YYYY"),
+        end_date: moment(new Date(2020, 7)).format("MMM YYYY"),
+        technologies: [
+            'C#',
+            'C++',
+            'Javascript',
+            '.NET',
+            'Entity Framework',
+            'SQL Server',
+            'Azure DevOps',
+            'Selenium WebDriver'
+        ],
+        notable_tasks: [
+            'Developed REST microservice for asynchronous messaging.',
+            'Implemented real-time encryption on a video call service.',
+            'Developed integration and functional automated testing, guaranteeing continuous integration.',
+            'Worked on development of VoIP call related functionalities using the SIP protocol'
         ]
     }
 ];
